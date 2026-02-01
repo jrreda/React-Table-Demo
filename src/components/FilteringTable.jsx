4003fbb230f387805fb3736c7cac3bfae5c53061
Reda <mahmoudreda457@gmail.com>
2026-02-01 15:13:57 +0200

    GlobalFilter

---
+++ src/components/FilteringTable.jsx
@@ -0,0 +1,71 @@
+import "./table.css";
+import { COLUMNS } from "./columns";
+import { useTable, useSortBy, useGlobalFilter } from "react-table";
+import { useMemo } from "react";
+import MOCK_DATA from "../../data/MOCK_DATA.json";
+import { GlobalFilter } from "./GlobalFilter";
+
+export const FilteringTable = () => {
+  const data = useMemo(() => MOCK_DATA, []);
+  const columns = useMemo(() => COLUMNS, []);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    footerGroups,
+    rows,
+    prepareRow,
+    state,
+    setGlobalFilter,
+  } = useTable({ columns, data }, useGlobalFilter, useSortBy);
+
+  const { globalFilter } = state;
+
+  return (
+    <>
+      <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
+      <table>
+        <thead {...getTableProps()}>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render("Header")}
+                  <span>
+                    {column.isSorted
+                      ? column.isSortedDesc
+                        ? " 🔽"
+                        : " 🔼"
+                      : ""}
+                  </span>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => (
+                  <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
+        <tfoot>
+          {footerGroups.map((footerGroup) => (
+            <tr {...footerGroup.getFooterGroupProps()}>
+              {footerGroup.headers.map((column) => (
+                <td {...column.getFooterProps()}>{column.render("Footer")}</td>
+              ))}
+            </tr>
+          ))}
+        </tfoot>
+      </table>
+    </>
+  );
+};
---
+++ src/components/GlobalFilter.jsx
@@ -0,0 +1,11 @@
+export const GlobalFilter = ({ filter, setFilter }) => {
+  return (
+    <div>
+      <input
+        type="text"
+        value={filter || ""}
+        onChange={(e) => setFilter(e.target.value)}
+      />
+    </div>
+  )
+}
